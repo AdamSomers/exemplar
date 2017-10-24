a8553caf5c4093a24d69ef4330534038453905fa
Adam Somers <adam@jauntvr.com>
2017-10-24 09:44:13 -0700

    comment for minheap

--- src/minheap.h
+++ src/minheap.h
@@ -5,6 +5,20 @@
 using namespace std;
 
 // MinHeap
+// The smallest element will always be the root.
+// Allows for O(1) retrieval of smallest element.
+// 
+// The data structure is stored as a simple array, with formulae
+// to retrieve indices for parent, left child & right child nodes.
+// 
+// New items are simply added to the end and bubbled up the tree
+// until a smaller parent is reached. This is done by iteratively
+// swapping with parents.
+//
+// When the smallest item is removed, the "heapfiy" operation restores
+// the min heap. This is done by moving the last item to the root and
+// recursively swapping the item with the smaller of its children until
+// both children are larger.
 
 namespace exemplar
 {
